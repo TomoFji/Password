abdf616a92b6eae6d791dc09a454542439159dc7
Tomasz Kwiatkowski <tomaszkwiatkowski191@gmail.com>
2023-09-22 19:45:35 +0200

    first commit

---
+++ pass.py
@@ -0,0 +1,25 @@
+import string, itertools, time
+
+def password_cracker(password):
+    characters = string.ascii_letters + string.digits + "!@#$%^&*()-_=+[]{}|;:\",.<>/?`~\\"
+    attempts = 0
+    
+    for pass_length in range (1,9):
+        for permutation in itertools.product(characters, repeat=pass_length):
+            attempts += 1
+            permutation = ''.join(permutation)
+            if permutation == password:
+                print(f"Password {password} guessed in {attempts} attempts")
+                return
+start = time.time()
+
+passwords = []
+with open("Passwords.txt", 'r') as file:
+    for line in file:
+        passwords.append(line.strip())
+
+for password in passwords:
+    password_cracker(password)
+
+end = time.time()
+print(end - start)
