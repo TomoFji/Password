e29b8a60cd7928fcfdf121413de36dddbca515d6
Tomasz Kwiatkowski <tomaszkwiatkowski191@gmail.com>
2023-09-24 19:23:28 +0200

    m

--- pass.py
+++ pass.py
@@ -1,16 +1,16 @@
 import string, itertools, time
 
 def password_cracker(password):
-    #function creates every single possible password permutation 
+    #function creates every single possible password combination 
     #until it lands on the correct one
     characters = string.ascii_letters + string.digits + "!@#$%^&*()-_=+[]{}|;:\",.<>/?`~\\"
     attempts = 0
     
     for pass_length in range (1,9):
-        for permutation in itertools.product(characters, repeat=pass_length):
+        for combination in itertools.product(characters, repeat=pass_length):
             attempts += 1
-            permutation = ''.join(permutation)
-            if permutation == password:
+            combination = ''.join(combination)
+            if combination == password:
                 print(f"Password {password} guessed in {attempts} attempts")
                 return
     print("Failure")
